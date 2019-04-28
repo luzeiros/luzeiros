5207d9746aec900e73707ac45247a24482cde7fd
Leo <oleoneto@gmail.com>
2019-04-28 18:30:39 -0400

    Remove articles from user endpoint

--- luzeiros/core/serializers/user.py
+++ luzeiros/core/serializers/user.py
@@ -6,15 +6,6 @@ from luzeiros.blog.serializers.comment import CommentSerializer
 
 class UserSerializer(serializers.ModelSerializer):
 
-    articles = serializers.SerializerMethodField()
-    comments = CommentSerializer(many=True, allow_null=True)
-
     class Meta:
         model = User
         exclude = ('password', 'is_superuser', 'is_staff', 'last_login', 'user_permissions')
-
-    class JSONAPIMeta:
-        included_resources = ['articles', 'comments']
-
-    def get_articles(self, obj):
-        return obj.articles.exclude(is_private=True)
