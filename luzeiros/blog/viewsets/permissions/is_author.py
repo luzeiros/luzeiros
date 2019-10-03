c42d350dd9cfecb5c0d2ec140bea2b3222a1e7e1
Leo <oleoneto@gmail.com>
2019-10-03 02:14:12 -0400

    Make authentication optional for article serializer

--- luzeiros/blog/viewsets/permissions/is_author.py
+++ luzeiros/blog/viewsets/permissions/is_author.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 
 
-class IsAuthorOrReadOnly(permissions.IsAuthenticated):
+class IsAuthorOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
