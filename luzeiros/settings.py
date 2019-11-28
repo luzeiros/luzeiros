2c50b43d3f2ec198336db399a3ad55eb4a8cfaac
Leo Neto <oleoneto@gmail.com>
2019-11-27 19:56:26 -0500

    REST: add pagination; disable browsable api

--- luzeiros/settings.py
+++ luzeiros/settings.py
@@ -254,9 +254,17 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 
 REST_FRAMEWORK = {
+    'PAGE_SIZE': 100,
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
 
 SIMPLE_JWT = {
