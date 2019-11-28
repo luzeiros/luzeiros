0597fd29ba93b553650d414dcef6498d9f93f168
Leo Neto <oleoneto@gmail.com>
2019-11-27 19:04:47 -0500

    Require authentication for podcast api view

--- luzeiros/radio/viewsets/program.py
+++ luzeiros/radio/viewsets/program.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework import permissions
 from luzeiros.radio.models.program import Program
 from luzeiros.radio.serializers.program import ProgramSerializer
 
@@ -6,3 +7,4 @@ from luzeiros.radio.serializers.program import ProgramSerializer
 class ProgramViewSet(viewsets.ModelViewSet):
     queryset = Program.objects.all()
     serializer_class = ProgramSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
